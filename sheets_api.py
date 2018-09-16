c05459ee1210ec4b27c8de37227a420bae090c0c
Scott <scott2345@gmail.com>
2018-09-16 10:09:16 -0700

    updating variables for prod environment.  also fixed some input nuiances.

--- sheets_api.py
+++ sheets_api.py
@@ -12,16 +12,27 @@ SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
 SPREADSHEET_ID = '1NH1i_nfPKhXO53uKYgJYICrTx_XSqDC88b2I3e0vsc0'
 RANGE_NAME = 'Tabulated Summary+Thoughts!A:K'
 HEADER_RANGE = 'Tabulated Summary+Thoughts!A2:K2'
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+
+try:
+    if os.environ['PRODUCTION'] == True:
+        TOKEN_PATH = os.path.normpath('/home/atraeus/eternal_draftbuddy/token.json')
+        CRED_PATH = os.path.normpath('/home/atraeus/eternal_draftbuddy/credentials.json')
+    else:
+        TOKEN_PATH = os.path.join(THIS_FOLDER, 'token.json')
+        CRED_PATH = os.path.join(THIS_FOLDER,'credentials.json')
+except KeyError as e:
+    TOKEN_PATH = os.path.join(THIS_FOLDER, 'token.json')
+    CRED_PATH = os.path.join(THIS_FOLDER,'credentials.json')
 
 def get_tdc_info():
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
     """
-    THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-    store = file.Storage(os.path.join(THIS_FOLDER, 'token.json'))
+    store = file.Storage(TOKEN_PATH)
     creds = store.get()
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets(os.path.join(THIS_FOLDER,'credentials.json'), SCOPES)
+        flow = client.flow_from_clientsecrets(CRED_PATH, SCOPES)
         creds = tools.run_flow(flow, store)
     service = build('sheets', 'v4', http=creds.authorize(Http()))
 
